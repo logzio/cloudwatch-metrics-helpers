32eefa3443939552ad97b7643d79c078349eeced
mirii1994 <miri.ignatiev@logz.io>
2022-11-20 15:50:44 +0200

    working code

--- tags/main.go
+++ tags/main.go
@@ -1,19 +1,27 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/resourcegroupstaggingapi"
+	metricsExporter "github.com/logzio/go-metrics-sdk"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/metric"
+	"go.opentelemetry.io/otel/sdk/metric/controller/basic"
 	"log"
 	"os"
 	"strings"
+	"time"
 )
 
 const (
-	envAwsRegion     = "AWS_REGION" // reserved env
-	envAwsNamespaces = "AWS_NAMESPACES"
+	envAwsRegion             = "AWS_REGION" // reserved env
+	envAwsNamespaces         = "AWS_NAMESPACES"
+	envLogzioMetricsListener = "LOGZIO_METRICS_LISTENER"
+	envLogzioMetricsToken    = "LOGZIO_METRICS_TOKEN"
 
 	emptyString   = ""
 	listSeparator = ","
@@ -25,33 +33,47 @@ func main() {
 	lambda.Start(HandleRequest)
 }
 
-func HandleRequest() error {
-	services, client, err := initialize()
+func HandleRequest(ctx context.Context) error {
+	services, client, exporter, err := initialize()
 	if err != nil {
 		return err
 	}
 
+	defer func() {
+		handleErr(exporter.Stop(ctx))
+	}()
+	meter := exporter.Meter("aws_resource_info")
+	intUpDownCounter := metric.Must(meter).NewInt64UpDownCounter("aws_resource_info")
+
 	var nextToken *string
+	nextToken = nil
+	resourcesPerPage := int64(100)
+	callsCounter := 0
 	for {
-		getResourcesOutput, err := client.GetResources(&resourcegroupstaggingapi.GetResourcesInput{
+		input := resourcegroupstaggingapi.GetResourcesInput{
 			PaginationToken:     nextToken,
 			ResourceTypeFilters: services,
-		})
+			ResourcesPerPage:    &resourcesPerPage,
+		}
+
+		getResourcesOutput, err := client.GetResources(&input)
+		callsCounter += 1
 
 		if err != nil {
-			return fmt.Errorf("error occurred while trying to get resources: ", err.Error())
+			return fmt.Errorf("error occurred while trying to get resources: %s", err.Error())
 		}
 
 		if getResourcesOutput != nil {
 			nextToken = getResourcesOutput.PaginationToken
-			sendTags(getResourcesOutput.ResourceTagMappingList)
+			sendTags(ctx, getResourcesOutput.ResourceTagMappingList, &intUpDownCounter)
 		}
 
-		if nextToken == nil {
+		if *nextToken == emptyString {
 			break
 		}
 	}
 
+	logger.Println("finished lambda run")
 	return nil
 }
 
@@ -82,30 +104,108 @@ func getSession() (*session.Session, error) {
 	return sess, nil
 }
 
-func initialize() ([]*string, *resourcegroupstaggingapi.ResourceGroupsTaggingAPI, error) {
+func initialize() ([]*string, *resourcegroupstaggingapi.ResourceGroupsTaggingAPI, *basic.Controller, error) {
 	namespaces, err := getAwsNamespaces()
 	if err != nil {
-		return nil, nil, fmt.Errorf("could not retrieve namespaces: %s. Aborting", err.Error())
+		return nil, nil, nil, fmt.Errorf("could not retrieve namespaces: %s. Aborting", err.Error())
 	}
 
 	services := namespacesToServices(namespaces)
 	if len(services) == 0 {
-		return nil, nil, fmt.Errorf("could not translate any namespace to service. Aborting")
+		return nil, nil, nil, fmt.Errorf("could not translate any namespace to service. Aborting")
 	}
 
 	sess, err := getSession()
 	if err != nil {
-		return nil, nil, fmt.Errorf("could not create AWS session: %s. Aborting", err.Error())
+		return nil, nil, nil, fmt.Errorf("could not create AWS session: %s. Aborting", err.Error())
 	}
 
 	tagsClient := resourcegroupstaggingapi.New(sess)
 	if tagsClient == nil {
-		return nil, nil, fmt.Errorf("could not initialize resource groups tags client. Aborting")
+		return nil, nil, nil, fmt.Errorf("could not initialize resource groups tags client. Aborting")
 	}
 
-	return services, tagsClient, nil
+	exporter, err := configureMetricsExporter()
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	return services, tagsClient, exporter, nil
 }
 
-func sendTags(list []*resourcegroupstaggingapi.ResourceTagMapping) {
+func configureMetricsExporter() (*basic.Controller, error) {
+	listener, err := getListener()
+	if err != nil {
+		return nil, fmt.Errorf("error while configuring metrics exporter: %s", err.Error())
+	}
 
+	token, err := getLogzioToken()
+	if err != nil {
+		return nil, fmt.Errorf("error while configuring metrics exporter: %s", err.Error())
+	}
+
+	config := metricsExporter.Config{
+		LogzioMetricsListener: listener,
+		LogzioMetricsToken:    token,
+		RemoteTimeout:         30 * time.Second,
+		PushInterval:          2 * time.Second,
+	}
+
+	exporter, err := metricsExporter.InstallNewPipeline(config, basic.WithCollectPeriod(2*time.Second))
+	if err != nil {
+		return nil, fmt.Errorf("error while configuring metrics exporter: %s", err.Error())
+	}
+
+	return exporter, nil
+}
+
+func getListener() (string, error) {
+	listener := os.Getenv(envLogzioMetricsListener)
+	if listener == emptyString {
+		return emptyString, fmt.Errorf("%s must be set", envLogzioMetricsListener)
+	}
+
+	return listener, nil
+}
+
+func getLogzioToken() (string, error) {
+	listener := os.Getenv(envLogzioMetricsToken)
+	if listener == emptyString {
+		return emptyString, fmt.Errorf("%s must be set", envLogzioMetricsListener)
+	}
+
+	return listener, nil
+}
+
+func sendTags(ctx context.Context, resources []*resourcegroupstaggingapi.ResourceTagMapping, counter *metric.Int64UpDownCounter) {
+	for _, resource := range resources {
+		// Since GetResources returns all resources that ever had a tag, it can return a resource that is now without a tag.
+		// Skip a tag-less resource
+		if len(resource.Tags) == 0 {
+			continue
+		}
+
+		logger.Println("handling resource ", *resource.ResourceARN)
+		attributes := make([]attribute.KeyValue, 0)
+		for _, tag := range resource.Tags {
+			attributes = append(attributes, attribute.KeyValue{
+				Key:   attribute.Key(*tag.Key),
+				Value: attribute.StringValue(*tag.Value),
+			})
+		}
+
+		attributes = append(attributes, attribute.KeyValue{
+			Key:   "arn",
+			Value: attribute.StringValue(*resource.ResourceARN),
+		})
+
+		counter.Add(ctx, int64(1), attributes...)
+	}
+
+}
+
+func handleErr(err error) {
+	if err != nil {
+		logger.Println("encountered error: ", err)
+	}
 }
