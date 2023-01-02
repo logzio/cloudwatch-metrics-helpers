b6cfc3b756980f2b9fa1ea22b1723d3de66e574b
Yotam loewenbach <yotam.loewenbach@logz.io>
2023-01-02 13:14:27 +0200

    Add logzio exporter logic + makefile

--- s3-daily-metrics/main.go
+++ s3-daily-metrics/main.go
@@ -1,102 +1,198 @@
-package s3_daily_metrics
+package main
 
 import (
 	"context"
 	"fmt"
+	metricsExporter "github.com/logzio/go-metrics-sdk"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/metric"
+	"go.opentelemetry.io/otel/sdk/metric/controller/basic"
+	"os"
 	"time"
 
-	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/cloudwatch"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+const (
+	envLogzioMetricsListener = "LOGZIO_METRICS_LISTENER"
+	envLogzioMetricsToken    = "LOGZIO_METRICS_TOKEN"
+	fieldLogzioAgentVersion  = "logzio_agent_version"
 )
 
 func main() {
-	lambda.Start(getS3Metrics)
+	//lambda.Start(handleRequest)
+	request, err := handleRequest(context.Background())
+	if err != nil {
+		print(request)
+	}
 }
 
-func getS3Metrics(ctx context.Context) error {
-	// Create an AWS session
-	sess, err := session.NewSession()
+func handleRequest(ctx context.Context) (string, error) {
+	exporter, err := configureMetricsExporter()
 	if err != nil {
-		return fmt.Errorf("error creating AWS session: %s", err)
+		return "", err
+	}
+	defer func() {
+		handleErr(exporter.Stop(ctx))
+	}()
+	meter := exporter.Meter("aws_s3")
+	// Create a new AWS session
+	sess, err := session.NewSession(&aws.Config{Region: aws.String("us-east-1")})
+	if err != nil {
+		fmt.Printf(err.Error())
 	}
 
-	// Create a CloudWatch client
-	svc := cloudwatch.New(sess)
+	// Create a new CloudWatch client
+	cw := cloudwatch.New(sess)
 
-	// Set the start and end times for the metric data
-	now := time.Now()
-	endTime := now.Add(time.Hour * 24)
-	startTime := endTime.Add(time.Hour * -48)
+	// Create a new S3 client
+	s3Client := s3.New(sess)
 
-	// Set the CloudWatch namespace to "AWS/S3"
-	namespace := "AWS/S3"
-
-	// Set the metrics to retrieve
-	metrics := []string{
-		"NumberOfObjects",
-		"BucketSizeBytes",
+	// List all the buckets in the S3 namespace
+	buckets, err := s3Client.ListBuckets(&s3.ListBucketsInput{})
+	if err != nil {
+		return "", err
 	}
 
-	// Set the dimensions for the metric data
-	dimensions := []*cloudwatch.Dimension{
-		{
-			Name:  aws.String("BucketName"),
-			Value: aws.String("*"),
-		},
-		{
-			Name:  aws.String("StorageType"),
-			Value: aws.String("*"),
-		},
-	}
-
-	// Create a CloudWatch GetMetricDataInput struct
-	input := &cloudwatch.GetMetricDataInput{
-		EndTime:   &endTime,
-		StartTime: &startTime,
-		MetricDataQueries: []*cloudwatch.MetricDataQuery{
-			{
-				Id: aws.String("m1"),
-				MetricStat: &cloudwatch.MetricStat{
-					Metric: &cloudwatch.Metric{
-						Namespace:  &namespace,
-						MetricName: &metrics[0],
-						Dimensions: dimensions,
+	// Iterate through the buckets and get the NumberOfObjects and BucketSizeBytes metrics for each bucket
+	for _, bucket := range buckets.Buckets {
+		// Get the NumberOfObjects metric for the bucket
+		numberOfObjectsMetric, err := cw.GetMetricData(&cloudwatch.GetMetricDataInput{
+			MetricDataQueries: []*cloudwatch.MetricDataQuery{
+				{
+					Id: aws.String("m1"),
+					MetricStat: &cloudwatch.MetricStat{
+						Metric: &cloudwatch.Metric{
+							Namespace:  aws.String("AWS/S3"),
+							MetricName: aws.String("NumberOfObjects"),
+							Dimensions: []*cloudwatch.Dimension{
+								{
+									Name:  aws.String("BucketName"),
+									Value: bucket.Name,
+								},
+								{
+									Name:  aws.String("StorageType"),
+									Value: aws.String("AllStorageTypes"),
+								},
+							},
+						},
+						Period: aws.Int64(86400),
+						Stat:   aws.String("Average"),
+						Unit:   aws.String("Count"),
 					},
-					Period: aws.Int64(3600),
-					Stat:   aws.String("Average"),
 				},
 			},
-			{
-				Id: aws.String("m2"),
-				MetricStat: &cloudwatch.MetricStat{
-					Metric: &cloudwatch.Metric{
-						Namespace:  &namespace,
-						MetricName: &metrics[1],
-						Dimensions: dimensions,
+			StartTime: aws.Time(time.Now().Add(-48 * time.Hour)),
+			EndTime:   aws.Time(time.Now()),
+		})
+		if err != nil {
+			return "", err
+		}
+		if len(numberOfObjectsMetric.MetricDataResults) > 0 {
+			numberOfObjects := int64(*numberOfObjectsMetric.MetricDataResults[0].Values[0])
+			prometheusNumberOfObjectsMetric := metric.Must(meter).NewInt64UpDownCounter("NumberOfObjects")
+			bucketNameAtt := attribute.KeyValue{
+				Key:   "bucket_name",
+				Value: attribute.StringValue(*bucket.Name),
+			}
+			prometheusNumberOfObjectsMetric.Add(ctx, numberOfObjects, bucketNameAtt)
+		}
+
+		// Get the BucketSizeBytes metric for the bucket
+		bucketSizeBytesMetric, err := cw.GetMetricData(&cloudwatch.GetMetricDataInput{
+			MetricDataQueries: []*cloudwatch.MetricDataQuery{
+				{
+					Id: aws.String("m2"),
+					MetricStat: &cloudwatch.MetricStat{
+						Metric: &cloudwatch.Metric{
+							Namespace:  aws.String("AWS/S3"),
+							MetricName: aws.String("BucketSizeBytes"),
+							Dimensions: []*cloudwatch.Dimension{
+								{
+									Name:  aws.String("BucketName"),
+									Value: bucket.Name,
+								},
+								{
+									Name:  aws.String("StorageType"),
+									Value: aws.String("StandardStorage"),
+								},
+							},
+						},
+						Period: aws.Int64(86400),
+						Stat:   aws.String("Average"),
+						Unit:   aws.String("Bytes"),
 					},
-					Period: aws.Int64(3600),
-					Stat:   aws.String("Average"),
 				},
 			},
-		},
-		MaxDatapoints: aws.Int64(100),
-	}
-
-	// Call the CloudWatch GetMetricData API
-	result, err := svc.GetMetricData(input)
-	if err != nil {
-		return fmt.Errorf("error calling CloudWatch GetMetricData API: %s", err)
-	}
-
-	// Print the metric data
-	for _, metricDataResult := range result.MetricDataResults {
-		fmt.Printf("Metric: %s\n", *metricDataResult.Label)
-		for _, datapoint := range metricDataResult.Values {
-			fmt.Printf("Timestamp: %s, Value: %f\n", *datapoint.Timestamp, *datapoint.Value)
+			StartTime: aws.Time(time.Now().Add(-48 * time.Hour)),
+			EndTime:   aws.Time(time.Now()),
+		})
+		if err != nil {
+			return "", err
+		}
+		if len(bucketSizeBytesMetric.MetricDataResults) > 0 {
+			bucketSizeBytes := int64(*bucketSizeBytesMetric.MetricDataResults[0].Values[0])
+			prometheusBucketSizeBytesMetric := metric.Must(meter).NewInt64UpDownCounter("BucketSizeBytes")
+			bucketNameAtt := attribute.KeyValue{
+				Key:   "bucket_name",
+				Value: attribute.StringValue(*bucket.Name),
+			}
+			prometheusBucketSizeBytesMetric.Add(ctx, bucketSizeBytes, bucketNameAtt)
 		}
 	}
 
-	return nil
+	return "Success", nil
+}
+
+func configureMetricsExporter() (*basic.Controller, error) {
+	listener, err := getListener()
+	if err != nil {
+		return nil, fmt.Errorf("error while configuring metrics exporter: %s", err.Error())
+	}
+
+	token, err := getLogzioToken()
+	if err != nil {
+		return nil, fmt.Errorf("error while configuring metrics exporter: %s", err.Error())
+	}
+
+	config := metricsExporter.Config{
+		LogzioMetricsListener: listener,
+		LogzioMetricsToken:    token,
+		RemoteTimeout:         30 * time.Second,
+		PushInterval:          2 * time.Second,
+	}
+
+	exporter, err := metricsExporter.InstallNewPipeline(config, basic.WithCollectPeriod(2*time.Second))
+	if err != nil {
+		return nil, fmt.Errorf("error while configuring metrics exporter: %s", err.Error())
+	}
+
+	return exporter, nil
+}
+
+func getListener() (string, error) {
+	listener := os.Getenv(envLogzioMetricsListener)
+	if listener == "" {
+		return "", fmt.Errorf("%s must be set", envLogzioMetricsListener)
+	}
+
+	return listener, nil
+}
+
+func getLogzioToken() (string, error) {
+	listener := os.Getenv(envLogzioMetricsToken)
+	if listener == "" {
+		return "", fmt.Errorf("%s must be set", envLogzioMetricsListener)
+	}
+
+	return listener, nil
+}
+
+func handleErr(err error) {
+	if err != nil {
+		fmt.Println("encountered error: ", err)
+	}
 }
